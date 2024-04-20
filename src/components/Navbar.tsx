ab7fd26e9fc2591ef15f0cf6d5f2c3ab43c30937
anntasziii <anntesziii.kushnir@gmail.com>
2024-04-21 00:04:47 +0200

    signIn/signOut

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignInButton, UserButton, SignedOut } from "@clerk/nextjs"
+import Link from "next/link"
+
+export default function Navbar() {
+    return <header className="shadow">
+        <div className="max-w-5xl mx-auto h-14 p-3 flex items-center justify-between front-medium">
+            <Link href="/">New meeting</Link>
+            <SignedIn>
+                <div className="flex items-center gap-5">
+                    <Link href="/meetings">Meetings</Link>
+                    <UserButton />
+                </div>
+            </SignedIn>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+        </div>
+    </header>
+}
---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+    publicRoutes: ["/meeting/:id*"]
+});
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"], 
+};
