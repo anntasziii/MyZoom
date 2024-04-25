bc7587eeccd83506496bf8fda2d715accefef7da
anntasziii <anntesziii.kushnir@gmail.com>
2024-04-25 11:52:03 +0200

    Finish

--- src/app/meeting/[id]/page.tsx
+++ src/app/meeting/[id]/page.tsx
@@ -1,16 +1,30 @@
-import { Metadata } from "next"
+import { currentUser } from "@clerk/nextjs/server";
+import { Metadata } from "next";
+import MeetingLoginPage from "./MeetingLoginPage";
 import MeetingPage from "./MeetingPage";
 
 interface PageProps {
-    params: { id: string}
+  params: { id: string };
+  searchParams: { guest: string };
 }
 
 export function generateMetadata({ params: { id } }: PageProps): Metadata {
-    return {
-      title: `Meeting ${id}`,
-    };
+  return {
+    title: `Meeting ${id}`,
+  };
 }
 
-export default function Page({params: {id}}:PageProps){
-    return <MeetingPage id={id} />
-} 
+export default async function Page({
+  params: { id },
+  searchParams: { guest },
+}: PageProps) {
+  const user = await currentUser();
+
+  const guestMode = guest === "true";
+
+  if (!user && !guestMode) {
+    return <MeetingLoginPage />;
+  }
+
+  return <MeetingPage id={id} />;
+}
